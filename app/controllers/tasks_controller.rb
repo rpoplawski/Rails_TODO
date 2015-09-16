40952af1aed3966806e6cf8a68c42925c3f00fff
Ryan Poplawski <claydewd@gmail.com>
2015-09-16 17:10:58 -0500

    'create' method added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,18 +19,29 @@ class TasksController < ApplicationController
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
   end
-end
 
-def create
-    #For create to work, we must *require* that a parameter is sent with the name 'message'
-    if params[:message].nil? || params[:message].empty?
+  def create
+    if params[:body].nil? || params[:body].empty?
       err_msg = "The 'message' parameter was empty or not found"
       render json: { error_msg: err_msg }.to_json, status: 422
     else
-      # todo = Todo.create(message: params[:message])
-      todo = Todo.new
-      todo.message = params[:message]
-      todo.save
-      render json: todo.to_json, status: 201
+      # task = Task.create(message: params[:message]) => this is the same as the (3) lines below
+      task = Task.new
+      task.body = params[:body]
+      task.save
+      render json: task.to_json, status: 201
     end
   end
+
+  def destroy
+    if Task.exists?(params[:id])
+      task = Task.find(params[:id])
+      task.destroy
+      render json: { message: "Task Deleted Successfully." }, status: 200
+    else
+      render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
+    end
+  end
+
+end
+
