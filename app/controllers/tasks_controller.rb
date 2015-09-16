4de4cac6aeda34bf7c2e15a20152bdd3c5747a08
Ryan Poplawski <claydewd@gmail.com>
2015-09-16 16:37:08 -0500

    updated method in'show'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,16 +21,16 @@ class TasksController < ApplicationController
   end
 end
 
-#def create
-#    #For create to work, we must *require* that a parameter is sent with the name 'message'
-#    if params[:message].nil? || params[:message].empty?
-#      err_msg = "The 'message' parameter was empty or not found"
-#      render json: { error_msg: err_msg }.to_json, status: 422
-#    else
-#      # todo = Todo.create(message: params[:message])
-#      todo = Todo.new
-#      todo.message = params[:message]
-#      todo.save
-#      render json: todo.to_json, status: 201
-#    end
-#  end
+def create
+    #For create to work, we must *require* that a parameter is sent with the name 'message'
+    if params[:message].nil? || params[:message].empty?
+      err_msg = "The 'message' parameter was empty or not found"
+      render json: { error_msg: err_msg }.to_json, status: 422
+    else
+      # todo = Todo.create(message: params[:message])
+      todo = Todo.new
+      todo.message = params[:message]
+      todo.save
+      render json: todo.to_json, status: 201
+    end
+  end
