09788cd4f515facd4e3f7a95e7943cd6983bef17
Ryan Poplawski <claydewd@gmail.com>
2015-09-17 10:50:35 -0500

    changing config files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,19 @@
 class TasksController < ApplicationController
 
   def index
+    #render text: "params : #{params.inspect}"
     tasks = Task.all
     render json: tasks.to_json, status: 200
   end
 
   def new
+    #render text: "params : #{params.inspect}"
     render json: Task.new
   end
 
 
   def show
-
+    #render text: "params : #{params.inspect}"
     if Task.exists?(params[:id])
       tasks = Task.find(params[:id])
       render json: tasks.to_json, status: 200
@@ -21,6 +23,7 @@ class TasksController < ApplicationController
   end
 
   def create
+    #render text: "params : #{params.inspect}"
     if params[:body].nil? || params[:body].empty?
       err_msg = "The 'message' parameter was empty or not found"
       render json: { error_msg: err_msg }.to_json, status: 422
@@ -34,6 +37,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    #render text: "params : #{params.inspect}"
     if Task.exists?(params[:id])
       task = Task.find(params[:id])
       task.destroy
@@ -44,15 +48,20 @@ class TasksController < ApplicationController
   end
 
   def update
-    if Task.exists?(params[:id])  && (params[:completion_level] == true)
-      task = ask.find(params[:id])
-      task.message = params[:message]
-      task.save
-      render json: task.to_json, status: 200
-    else
-      render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
-    end
-  end
+    #render text: "params : #{params.inspect}"
+     if Task.exists?(params[:id])
+      update_task_status = Task.update(params[:id], completed: params[:completion_level])
 
+      render json: update_task_status, status: 200
+
+    # if Task.exists?(params[:id]) && (params[:completion_level] == true)
+    #    task = Task.find(params[:id])
+    #   task.completion_level = params[:completion_level]
+    #   task.save
+    #   render json: task.to_json, status: 200
+     else
+      render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
+     end
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-Rails.application.routes.draw do
-  get    '/tasks',      to: 'tasks#index'
-  get    '/tasks/new',  to: 'tasks#new'
-  get    '/tasks/:id',  to: 'tasks#show'
-  post   '/tasks/',     to: 'tasks#create'
-  put    '/tasks/:id',  to: 'tasks#update'
-  delete '/tasks/:id',  to: 'tasks#destroy'
+Rails.application.routes.draw     do
+  get    '/tasks',             to: 'tasks#index'
+  get    '/tasks/new',         to: 'tasks#new'
+  get    '/tasks/:id',         to: 'tasks#show'
+  post   '/tasks/',            to: 'tasks#create'
+  put    '/tasks/:id',         to: 'tasks#update'
+  delete '/tasks/:id',         to: 'tasks#destroy'
 end
 
 
